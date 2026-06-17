ffbfd4676fec189e3105007b06a9fff0d124801d
JJGebel <mikeboss5555@gmail.com>
2026-06-17 21:40:59 +0200

    Created crude version of chat ui

---
+++ chatmain.php
@@ -0,0 +1,44 @@
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="chatmain.css">
+    <title>AdamedDT</title>
+    <script src="chat.js" defer></script>
+</head>
+<body>    
+    
+    <div class="content">
+
+        <div class="chat wrapper">
+        
+            <div class="chat-container">
+
+                <div class="chat-view">
+                    
+                    <div class="bubble sent">
+                        Wiadomosc do backendu
+                    </div>
+
+                    
+                    <div class="bubble received">
+                        oodpowiedz od chat gpt
+                    </div>
+                </div>
+
+                <div class="chat-prompt">
+                    <input type="text" placeholder="WRITE PROMPT HERE..." autocomplete="off">
+                </div>
+
+            </div>
+
+        </div>
+
+        <div class="button-proceed">
+            <button class="end-chat-btn">Koniec rozmowy</button>
+        </div>  
+
+    </div>
+
+</body>
+</html>
