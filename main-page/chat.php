f9e73b82d61d454283f3f5d3966213f6e6a7a89a
JJGebel <mikeboss5555@gmail.com>
2026-06-19 00:56:55 +0200

    Fixed chat css

--- main-page/chat.php
+++ main-page/chat.php
@@ -3,32 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="chatmain.css">
+    <link rel="stylesheet" href="chat.css">
     <title>AdamedDT</title>
     <script src="chat.js" defer></script>
 </head>
 <body>
-    <div class="header">
-        <div class="header-left">
-            <div class="profile-wrapper">
-                <div class="profile-main">
-                    <div class="profile-image"></div>
-                    <div class="profile-tag"><span>admin</span></div>
-                </div>
-            </div>
-        </div>
-        
-        <div class="header-right">
-            <div class="settings-wrapper">
-                <button class="settings-main">
-                    <div class="settings-icon-outer">
-                        <div class="settings-icon-inner"></div>
-                    </div>
-                </button>
-            </div>
-        </div>
-    </div>
-
     <div class="content">
         <div class="chat wrapper">
             <div class="chat-container">
